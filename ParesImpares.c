63980b1afe6fd827d1a2e9946ea3807f0696985c
Israel-SoaresPorto <isaelporto15@gmail.com>
2023-05-26 15:25:48 -0300

    change code

--- ParesImpares.c
+++ ParesImpares.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 int main()
 {
-    setlocale(LC_ALL, "");
 
     int numero, pares = 0, impares = 0;
     int somaDePares; 
-    float mediaDosPares;
+    int mediaDosPares;
 
     do {
+
         printf("digite um número: ");
         scanf("%d", &numero);
 
         fflush(stdin);
 
-        if(numero % 2 == 0) {
-            pares++;
-            somaDePares += numero;
-        } else {
-            impares++;
+        if (numero > 0) {
+            if(numero % 2 == 0) {
+                pares++;
+                somaDePares += numero;
+            } else {
+                impares++;
+            }
         }
+
     } while(numero > 0);
 
     mediaDosPares = somaDePares / pares;
 
     printf("quantidade de números pares: %d\n", pares);
     printf("quantidade de números impares: %d\n", impares);
-    printf("média dos pares: %.2f", mediaDosPares);
+    printf("média dos pares: %d", mediaDosPares);
    
     return 0;
 }
