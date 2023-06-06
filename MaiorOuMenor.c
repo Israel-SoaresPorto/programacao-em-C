4106bf85d2b6dd83ba4b075d7362feb8becd755a
Israel-SoaresPorto <isaelporto15@gmail.com>
2023-06-06 16:51:29 -0300

    modified 06/06

--- MaiorOuMenor.c
+++ MaiorOuMenor.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <locale.h>
 
+int maiorValor(int n[])
+{
+    int maior = 0, i;
+
+    for (i = 0; i < 5; i++)
+    {
+        if(n[i] > maior)
+        {
+            maior = n[i];
+        }
+    }
+
+    return maior;
+}
+
+int menorValor(int n[])
+{
+    int menor = __INT_MAX__, i;
+
+    for (i = 0; i < 5; i++)
+    {
+        if(n[i] < menor)
+        {
+            menor = n[i];
+        }
+    }
+
+    return menor;
+}
+
 int main()
 {
     setlocale(LC_ALL, "");
 
     int numeros[5];
-    int maior = 0;
-    int menor = __INT_MAX__;
     int i;
 
     for (i = 0; i < 5; i++)
@@ -17,15 +45,6 @@ int main()
 
         fflush(stdin);
 
-        if(numeros[i] > maior) 
-        {
-            maior = numeros[i];
-        }
-
-        if(numeros[i] < menor)
-        {
-            menor = numeros[i];
-        }
     }
 
     printf("números: |");
@@ -36,8 +55,8 @@ int main()
     
     printf("\n");
 
-    printf("maior valor: %d \n", maior);
-    printf("menor: %d \n", menor);
+    printf("maior valor: %d \n", maiorValor(numeros));
+    printf("menor: %d \n", menorValor(numeros));
 
     return 0;
 }
